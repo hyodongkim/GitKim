67e7f5c3deddaebdb0c0e3cb116fa6a7b62ce62f
Leexxxxxx <ckzns@naver.com>
2021-10-08 15:52:57 +0900

    2021.10.08

--- portfolio/src/main/java/com/study/springboot/service/AdminService.java
+++ portfolio/src/main/java/com/study/springboot/service/AdminService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.study.springboot.dao.IAdminDao;
 import com.study.springboot.dao.IBookListDao;
 import com.study.springboot.dao.IBookReviewDao;
 import com.study.springboot.dao.IMemberDao;
@@ -15,11 +16,12 @@ import com.study.springboot.dto.BookReviewDto;
 import com.study.springboot.dto.MemberDto;
 import com.study.springboot.dto.NoticeDto;
 import com.study.springboot.dto.QnADto;
-import com.study.springboot.dto.QnA_AnswerDto;
 
 @Component
 public class AdminService {
 
+	@Autowired
+	private IAdminDao AdminDao;
 	@Autowired
 	private IMemberDao MemberDao;
 	@Autowired
@@ -31,19 +33,20 @@ public class AdminService {
 	@Autowired 
 	private IQnADao QnADao; 
 	
+	
+	
+	
+	
 	// 관리자 로그인
 	  public int adminLogin( String hp_ID, String hp_Password ) {
 		  int result = 0;
 		  
-		  if ( hp_ID == "admin" && hp_Password == "password" ) {
-			  int count = MemberDao.adminLogin( hp_ID, hp_Password );
-			  if( count > 0 ) {
-				  result = 1;
-			  }
-		  } else {
-			  result = 0;
+		  
+		  int count = AdminDao.adminLogin( hp_ID, hp_Password );
+		  if( count > 0 ) {
+			  result = 1;
 		  }
-
+		  
 		return result;
 	  }
 	
@@ -77,30 +80,18 @@ public class AdminService {
 		  
 	  }
 	  
-	// 공지사항 조회수 올리기
-	   public int countNotice(int count) throws Exception{
-		  
-		  return NoticeDao.countNotice(count);
-	   }
-	  
-	// 1:1 문의 목록
-	   public List<QnADto> QnAlist(){
+	  public List<QnADto> QnAlist(){
 		  List<QnADto> qnalist = QnADao.QnAlist();
 		  
 		  return qnalist;
 		  
-	   }
-
-	    // 1:1 문의 답안
-	    public List<QnA_AnswerDto> QnA_Answer( int qna_Index ) {
-		List<QnA_AnswerDto> qna_Answer = QnADao.QnA_Answer( qna_Index );
-		
-		return qna_Answer;
-	}
-	  
-	  // 1:1 문의 
+	  }
 	 
-	  
+	  // 공지사항 조회수 올리기
+	  public int countNotice(int count) throws Exception{
+		  
+		  return NoticeDao.countNotice(count);
+	  }
 	  
 	  
 	  
